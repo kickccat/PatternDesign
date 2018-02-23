b7ffc4f8a89ede13047fd599fd4cb97dedff25c0
Joey <kickccat@hotmail.com>
2018-02-23 17:41:26 +0100

    Add files via upload

---
+++ PatternDesign/FactoryDesign/PizzaSeller/src/service/NYPizzaIngredientFactory.java
@@ -0,0 +1,35 @@
+package service;
+
+import model.*;
+
+public class NYPizzaIngredientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new ThinCrustDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return null;
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return null;
+    }
+
+    @Override
+    public Veggies createVeggies() {
+        return null;
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return null;
+    }
+
+    @Override
+    public Clams createClams() {
+        return null;
+    }
+}
---
+++ PatternDesign/FactoryDesign/PizzaSeller/src/service/PizzaIngredientFactory.java
@@ -0,0 +1,14 @@
+package service;
+
+import model.*;
+
+public interface PizzaIngredientFactory {
+
+    Dough createDough();
+    Sauce createSauce();
+    Cheese createCheese();
+    Veggies createVeggies();
+    Pepperoni createPepperoni();
+    Clams createClams();
+
+}
