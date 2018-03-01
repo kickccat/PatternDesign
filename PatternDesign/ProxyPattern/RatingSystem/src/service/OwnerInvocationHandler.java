e8d88c093888b81fe066a0edbf5835ffd7a7b30d
Yi Zhou <kickccat@hotmail.com>
2018-03-02 00:06:19 +0100

    add Proxy pattern

---
+++ PatternDesign/ProxyPattern/RatingSystem/src/service/OwnerInvocationHandler.java
@@ -0,0 +1,32 @@
+package service;
+
+import model.PersonBean;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class OwnerInvocationHandler implements InvocationHandler {
+
+    PersonBean person;
+
+    public OwnerInvocationHandler(PersonBean person) {
+        this.person = person;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws IllegalAccessException {
+        try {
+            if (method.getName().startsWith("get")) {
+                return method.invoke(person, args);
+            } else if (method.getName().equals("setHotOrNotRating")) {
+                throw new IllegalAccessException("No manupulation right");
+            } else if (method.getName().startsWith("set")) {
+                return method.invoke(person, args);
+            }
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
